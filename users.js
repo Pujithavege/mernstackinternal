b3da6022134ab0b1207b20871f63c967eda4fe0c
Pujithavege <109502018+Pujithavege@users.noreply.github.com>
2024-03-15 15:18:24 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,19 @@
+const users=[
+{
+id:1,
+name:'Ram',
+email:'ram@gmail.com'
+},
+{
+    id:2,
+    name:'Priya',
+    email:'priya@gmail.com'
+    },
+ {
+        id:3,
+        name:'Tara',
+        email:'tara@gmail.com'
+}
+
+];
+module.exports=users;
